07182ba22dbba53a0d77d9ac2e7086b55b3d7979
Gabriela Calcabrine <gabrielacalcabrine@gmail.com>
2023-03-16 17:02:23 -0300

    Criacao Base Service interface

---
+++ Mda/Mda.Domain/Interfaces/IBaseService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mda.Domain.Interfaces
+{
+    public interface IBaseService<Request, Response>
+    {
+        Task<IEnumerable<Response>> Get();
+
+        Task<Response> GetById(int id);
+
+        Task<Response> Post(Request request);
+
+        Task<Response> Put(Request request, int? id);
+
+        Task Delete(int request);
+
+    }
+}
