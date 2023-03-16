5901d5d4a279002f420a4b9ab73dfc84719113f3
Gabriela Calcabrine <gabrielacalcabrine@gmail.com>
2023-03-16 20:04:13 -0300

    classe claimUtil criada

---
+++ Mda/Mda.Service/BaseService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mda.Service
+{
+    public class BaseService
+    {
+        public readonly Guid? UsuarioId;
+        private IHttpContextAccessor httpContextAccessor;
+        public BaseService(IHttpContextAccessor httpContextAccessor)
+        {
+            //UsuarioId = httpContextAccessor.HttpContext.GetClaim(ClaimTypes.NameIdentifier).ToInt();            
+        }
+    }
+}
