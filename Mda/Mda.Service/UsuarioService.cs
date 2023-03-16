8cf6352c9db3f9f9abce03efaab5d0c3ee4a6c29
Gabriela Calcabrine <gabrielacalcabrine@gmail.com>
2023-03-16 19:15:52 -0300

    Usuario request feito

---
+++ Mda/Mda.Service/UsuarioService.cs
@@ -0,0 +1,16 @@
+﻿using Mda.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mda.Service
+{
+   public class UsuarioService 
+    {
+        private readonly IUsuarioRepository _usuarioRepository;
+
+        
+    }
+}
