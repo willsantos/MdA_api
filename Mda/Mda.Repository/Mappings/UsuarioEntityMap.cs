4b3db1e948baa8a80c5f47f4c31a23be972b5f72
Gabriela Calcabrine <gabrielacalcabrine@gmail.com>
2023-03-16 19:29:29 -0300

    Usuario Entity Map criado

---
+++ Mda/Mda.Repository/Mappings/UsuarioEntityMap.cs
@@ -0,0 +1,25 @@
+﻿using Mda.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mda.Repository.Mappings
+{
+    public class UsuarioEntityMap
+    {
+        public void Configure(EntityTypeBuilder<Usuario> builder)
+        {
+            /*builder
+                .Property(prop => prop.Role)
+                .HasConversion(
+                    prop => prop.ToString(),
+            prop => (EnumRole)Enum.Parse(typeof(EnumRole), prop)
+            );*/ //vamos trabalhar com níveis de acesso?
+
+            builder.HasIndex(prop => prop.Email).IsUnique();
+        }
+    }
+}
