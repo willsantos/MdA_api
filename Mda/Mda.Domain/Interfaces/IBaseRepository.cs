fc2570f662cbcbee0c26ac15c58bd0382b84a0cc
Gabriela Calcabrine <gabrielacalcabrine@gmail.com>
2023-03-16 14:08:19 -0300

    Criação Base Repository e context

---
+++ Mda/Mda.Domain/Interfaces/IBaseRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mda.Domain.Interfaces
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        Task<T> FindAsync(int id);
+        Task<T> FindAsync(decimal id);
+        Task<T> FindAsync(Expression<Func<T, bool>> expression);
+        Task<T> FindAsNoTrackingAsync(Expression<Func<T, bool>> expression);
+        Task<int> CountAsync();
+        Task<int> CountAsync(Expression<Func<T, bool>> expression);
+        Task<int> CountAsync<K>(Expression<Func<T, IEnumerable<K>>> selectExpression);
+        Task<int> CountAsync<K>(Expression<Func<T, bool>> expression, Expression<Func<T, IEnumerable<K>>> selectExpression);
+        Task<IEnumerable<T>> ListAsync();
+        Task<IEnumerable<T>> ListAsync(Expression<Func<T, bool>> expression);
+        Task<List<T>> ListPaginationAsync<K>(Expression<Func<T, K>> sortExpression, int pagina, int quantidade);
+        Task<List<T>> ListPaginationAsync<K>(Expression<Func<T, bool>> expression, Expression<Func<T, K>> sortExpression, int pagina, int quantidade);
+        Task<T> AddAsync(T item);
+        Task RemoveAsync(T item);
+        Task<T> EditAsync(T item);
+
+    }
+}
---
+++ Mda/Mda.Repository/Repositories/BaseRepository.cs
@@ -0,0 +1,89 @@
+﻿using Mda.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mda.Repository.Repositories
+{
+    public abstract class BaseRepository<T> : IBaseRepository<T> where T : class 
+        // As classes abstratas são as que não permitem realizar qualquer tipo de instância.
+    {
+        public Task<T> AddAsync(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> CountAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> CountAsync(Expression<Func<T, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> CountAsync<K>(Expression<Func<T, IEnumerable<K>>> selectExpression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> CountAsync<K>(Expression<Func<T, bool>> expression, Expression<Func<T, IEnumerable<K>>> selectExpression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> EditAsync(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> FindAsNoTrackingAsync(Expression<Func<T, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> FindAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> FindAsync(decimal id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> FindAsync(Expression<Func<T, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> ListAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> ListAsync(Expression<Func<T, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<T>> ListPaginationAsync<K>(Expression<Func<T, K>> sortExpression, int pagina, int quantidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<T>> ListPaginationAsync<K>(Expression<Func<T, bool>> expression, Expression<Func<T, K>> sortExpression, int pagina, int quantidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveAsync(T item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
