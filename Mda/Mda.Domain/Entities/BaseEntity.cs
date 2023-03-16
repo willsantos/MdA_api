0de9bfbb7ecfd77fb5bf37dc67562e8460b8c454
Gabriela Calcabrine <gabrielacalcabrine@gmail.com>
2023-03-16 16:22:57 -0300

    Entidade base e entidade Usuario

---
+++ Mda/Mda.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mda.Domain.Entities
+{
+    public class BaseEntity
+    {
+        public BaseEntity()
+        {
+            Ativo = true;
+        }
+        public bool Ativo { get; set; }
+        public Guid Id = new Guid();
+    }
+}
