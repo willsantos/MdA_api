f56277112f4c9616ee26689881b29c10b577a3d3
Gabriela Calcabrine <gabrielacalcabrine@gmail.com>
2023-03-17 10:50:13 -0300

    Acrescimo de atributo Id no request de role para realizacao da busca

--- Mda/Mda.Domain/UsuarioContratos/UsuarioRequestRole.cs
+++ Mda/Mda.Domain/UsuarioContratos/UsuarioRequestRole.cs
@@ -1,6 +1,7 @@
 ﻿using Mda.Domain.Entities.Utils;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,8 @@ namespace Mda.Domain.UsuarioContratos
 {
     public class UsuarioRequestRole
     {
+        [Required(ErrorMessage = "Use Usuario ou Administrador para preencher a Role")]
         public EnumAcesso Role { get; set; }
+        public Guid Id { get; set; }
     }
 }
