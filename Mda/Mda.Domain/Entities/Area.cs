9bfd987d3bbd5829e7dde6f4d5fbee4d0dbe99fe
Gabriela Calcabrine <gabrielacalcabrine@gmail.com>
2023-03-22 12:52:30 -0300

    Criacao entidade Area

---
+++ Mda/Mda.Domain/Entities/Area.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mda.Domain.Entities
+{
+    public class Area : BaseEntity
+    {
+        public int Tipo { get; set; } // defino smallInt nas configs do Sql, certo?
+    }
+}
