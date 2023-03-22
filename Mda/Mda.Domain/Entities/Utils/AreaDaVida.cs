19e9b3c337d04563286a53853a6e61fb92e1fe00
Gabriela Calcabrine <gabrielacalcabrine@gmail.com>
2023-03-22 12:56:43 -0300

    Enum AreaDaVida criado

---
+++ Mda/Mda.Domain/Entities/Utils/AreaDaVida.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mda.Domain.Entities.Utils
+{
+    public enum AreaDaVida
+    {
+        DesenvolvimentoPessoal,
+        Profissional,
+        Finanças,
+        Lazer,
+        Relacionamento,
+        Casa,
+        Saúde,
+        FamiliaAmigos,
+        VidaSocial,
+        Espiritualidade,
+        Estudos,
+        EquilibrioEmocional,
+    }
+}
