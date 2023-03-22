6648654397daff8158cb9265b91264938fca3583
Gabriela Calcabrine <gabrielacalcabrine@gmail.com>
2023-03-22 13:06:14 -0300

    Criacao AreaEntityMap

---
+++ Mda/Mda.Repository/Mappings/AreaEntityMap.cs
@@ -0,0 +1,24 @@
+﻿using Mda.Domain.Entities;
+using Mda.Domain.Entities.Utils;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mda.Repository.Mappings
+{
+    public class AreaEntityMap
+    {
+        public void Configure(EntityTypeBuilder<Area> builder)
+        {
+            builder
+                .Property(prop => prop.Tipo)
+                .HasConversion(
+                    prop => prop.ToString(),
+            prop => (AreaDaVida)Enum.Parse(typeof(AreaDaVida), prop)
+            );
+        }
+    }
+}
