7d2ca911b807c2f508d0b9d98539d39887a32eed
Gabriela Calcabrine <gabrielacalcabrine@gmail.com>
2023-03-23 09:52:04 -0300

    Criacao ObjetivoRepository

--- Mda/Mda.Domain/Interfaces/IObjetivoRepository.cs
+++ Mda/Mda.Domain/Interfaces/IObjetivoRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Mda.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Mda.Domain.Interfaces
 {
-    public interface IObjetivoRepository : IBaseRepository<Object>
+    public interface IObjetivoRepository : IBaseRepository<Objetivo>
     {
     }
 }
---
+++ Mda/Mda.Repository/Repositories/ObjetivoRepository.cs
@@ -0,0 +1,18 @@
+﻿using Mda.Domain.Entities;
+using Mda.Domain.Interfaces;
+using Mda.Repository.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mda.Repository.Repositories
+{
+    public class ObjetivoRepository : BaseRepository<Objetivo>, IObjetivoRepository
+    {
+        public ObjetivoRepository(MdaContext context) : base(context)
+        {
+        }
+    }
+}
