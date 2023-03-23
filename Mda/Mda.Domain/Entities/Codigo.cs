2c5cc97349378ee0f86f42b4674c98bc669a6ed8
Gabriela Calcabrine <gabrielacalcabrine@gmail.com>
2023-03-23 11:14:02 -0300

    Criacao Entidade Codigo

---
+++ Mda/Mda.Domain/Entities/Codigo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mda.Domain.Entities
+{
+    public class Codigo
+    {
+        public Guid Id { get; set; }
+        public string Email { get; set; }
+        public string CodigoResgate { get; set; }
+        public DateTime GeradoEm { get; set; }
+    }
+}
