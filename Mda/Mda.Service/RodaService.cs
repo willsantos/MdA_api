c62e45ecb088b45deb25ffc2cd0284008be4e468
Gabriela Calcabrine <gabrielacalcabrine@gmail.com>
2023-03-23 17:22:37 -0300

    Lista de rodas

--- Mda/Mda.Service/RodaService.cs
+++ Mda/Mda.Service/RodaService.cs
@@ -2,13 +2,8 @@
 using Mda.Domain.Entities;
 using Mda.Domain.Interfaces;
 using Mda.Domain.UsuarioContratos;
-using Mda.Repository.Repositories;
 using Microsoft.AspNetCore.Http;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Buffers;
 
 namespace Mda.Service
 {
@@ -18,8 +13,8 @@ namespace Mda.Service
         private readonly IUsuarioRepository _usuarioRepository;
         private readonly IMapper _mapper;
 
-        public RodaService(IHttpContextAccessor httpContextAccessor, 
-                           IRodaRepository rodaRepository, 
+        public RodaService(IHttpContextAccessor httpContextAccessor,
+                           IRodaRepository rodaRepository,
                            IUsuarioRepository usuarioRepository,
                             IMapper mapper) : base(httpContextAccessor)
         {
@@ -31,6 +26,7 @@ namespace Mda.Service
         public async Task<RodaResponse> Post(RodaRequest request)
         {
             var usuario = await _usuarioRepository.FindAsync(x => x.Id == UsuarioId && x.Ativo == true);
+            //if(usuario.Rodas.Count() == 2)
             var roda = _mapper.Map<Roda>(request);
             roda.DataCriacao = DateTime.Now;
             roda.UsuarioId = (Guid)UsuarioId;
@@ -38,20 +34,25 @@ namespace Mda.Service
             return _mapper.Map<RodaResponse>(rodaCadastrada);
         }
 
+        public async Task<RodaResponse> GetById(Guid id)
+        {
+            var roda = await _rodaRepository.FindAsync(x => x.Id == id && x.UsuarioId == UsuarioId);
+            if (roda == null)
+            {
+                throw new Exception("A Roda buscada não existe");
+            }
+            return _mapper.Map<RodaResponse>(roda);
+        }
+
+        public async Task<IEnumerable<RodaResponse>> Get()
+        {
+            var listRodas = await _rodaRepository.ListAsync(x => x.UsuarioId == UsuarioId);
+            return _mapper.Map<IEnumerable<RodaResponse>>(listRodas);
+        }
         public Task Delete(Guid Id)
         {
             throw new NotImplementedException();
-        }
-
-        public Task<IEnumerable<RodaResponse>> Get()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<RodaResponse> GetById(Guid id)
-        {
-            throw new NotImplementedException();
-        }       
+        }      
 
         public Task<RodaResponse> Put(RodaRequest request, Guid? id)
         {
