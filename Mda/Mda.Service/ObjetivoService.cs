8e1ab6e9f5f3d2c0066106a0e27a40861a979815
Gabriela Calcabrine <gabrielacalcabrine@gmail.com>
2023-03-24 15:19:35 -0300

    ObjetivoService criado

---
+++ Mda/Mda.Service/ObjetivoService.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using Mda.Domain.Interfaces;
+using Mda.Domain.UsuarioContratos;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mda.Service
+{
+    public class ObjetivoService : BaseService, IBaseService<ObjetivoRequest, ObjetivoResponse>
+    {
+        private readonly IObjetivoRepository _objetivoRepository;
+        private readonly IUsuarioRepository _usuarioRepository;
+        private readonly IMapper _mapper;
+        public ObjetivoService(IHttpContextAccessor httpContextAccessor, 
+                               IUsuarioRepository usuarioRepository,
+                               IObjetivoRepository objetivoRepository, IMapper mapper) : base(httpContextAccessor)
+        {
+            _mapper = mapper;
+            _usuarioRepository = usuarioRepository;
+            _objetivoRepository = objetivoRepository;
+        }
+
+        public Task Delete(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ObjetivoResponse>> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ObjetivoResponse> GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ObjetivoResponse> Post(ObjetivoRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ObjetivoResponse> Put(ObjetivoRequest request, Guid? id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
