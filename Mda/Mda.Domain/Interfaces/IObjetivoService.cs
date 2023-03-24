e61473b629e63049a6898b2b9053d166e1f75484
Gabriela Calcabrine <gabrielacalcabrine@gmail.com>
2023-03-24 15:03:53 -0300

    ObjetivoRequest criado

---
+++ Mda/Mda.Domain/Interfaces/IObjetivoService.cs
@@ -0,0 +1,14 @@
+﻿using Mda.Domain.UsuarioContratos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mda.Domain.Interfaces
+{
+    public interface IObjetivoService : IBaseService<ObjetivoRequest, ObjetivoResponse>
+    {
+
+    }
+}
---
+++ Mda/Mda.Domain/UsuarioContratos/ObjetivoRequest.cs
@@ -0,0 +1,25 @@
+﻿using Mda.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mda.Domain.UsuarioContratos
+{
+    public class ObjetivoRequest
+    {
+        [Required(ErrorMessage = "Id de AREA é obrigatório")]
+        public Guid AreaId { get; set; }
+
+        [Required(ErrorMessage = "Data final é obrigatório")]
+        public DateTime DataFinal { get; set; }
+
+        [Required(ErrorMessage = "Bloqueios é obrigatório")]
+        public string Bloqueios { get; set; }
+
+        [Required(ErrorMessage = "Recursos é obrigatório")]
+        public string Recursos { get; set; }
+    }
+}
