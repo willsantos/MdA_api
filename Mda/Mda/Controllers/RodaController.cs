7d1e16c4515385d54f8b5f45040a02958d6a3ca1
Gabriela Calcabrine <gabrielacalcabrine@gmail.com>
2023-03-28 16:49:00 -0300

    feat : metodo post RodaController criado

---
+++ Mda/Mda/Controllers/RodaController.cs
@@ -0,0 +1,27 @@
+﻿using Mda.Domain.Interfaces;
+using Mda.Domain.UsuarioContratos;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Mda.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RodaController : ControllerBase
+    {
+        private readonly IRodaService _rodaService;
+
+        public RodaController(IRodaService rodaService)
+        {
+            _rodaService = rodaService;
+        }
+        [HttpPost]
+        [SwaggerOperation(Summary = "Cadastra um nova Roda no banco.", Description = "Retorna dados da Roda.")]
+        [ProducesResponseType(201)]
+        public async Task<ActionResult<RodaResponse>> Post([FromBody] RodaRequest roda)
+        {
+            var result = await _rodaService.Post(roda);
+            return Ok(result);
+        }
+    }
+}
