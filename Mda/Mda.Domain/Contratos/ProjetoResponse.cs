158b6862e0c10668b03b3cc932fb9a4c5bb6f059
Gabriela Calcabrine <gabrielacalcabrine@gmail.com>
2023-03-28 10:11:52 -0300

    Projeto Response realizado

--- Mda/Mda.Domain/Contratos/ProjetoResponse.cs
+++ Mda/Mda.Domain/Contratos/ProjetoResponse.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Mda.Domain.Entities.Utils;
+using Mda.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,10 @@ namespace Mda.Domain.Contratos
 {
     public class ProjetoResponse
     {
+        public Guid ObjetivoId { get; set; }        
+        public string Nome { get; set; }
+        public DateTime DataAlvo { get; set; }
+        public EnumStatus Status { get; set; }
+        public float Progresso { get; set; }
     }
 }
