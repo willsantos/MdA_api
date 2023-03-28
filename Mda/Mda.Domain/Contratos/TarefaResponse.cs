f2fcea52a655fe3a6ed04c2d7312feb5dff009d9
Gabriela Calcabrine <gabrielacalcabrine@gmail.com>
2023-03-28 12:07:23 -0300

    TarefaRequest e Response

---
+++ Mda/Mda.Domain/Contratos/TarefaResponse.cs
@@ -0,0 +1,11 @@
+﻿using Mda.Domain.Entities.Utils;
+using Mda.Domain.Entities;
+
+namespace Mda.Domain.Contratos
+{
+    public class TarefaResponse
+    {
+        public Guid ProjetoId { get; set; }        
+        public EnumStatus StatusTarefa { get; set; }
+    }
+}
