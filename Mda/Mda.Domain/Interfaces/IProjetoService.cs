854b8101701527222c7259ea9f7a261b0a4e1891
Gabriela Calcabrine <gabrielacalcabrine@gmail.com>
2023-03-28 10:13:31 -0300

    IProjetoInterface herdando IBaseService!

--- Mda/Mda.Domain/Interfaces/IProjetoService.cs
+++ Mda/Mda.Domain/Interfaces/IProjetoService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Mda.Domain.Contratos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Mda.Domain.Interfaces
 {
-    public class IProjetoService
+    public interface IProjetoService : IBaseService<ProjetoRequest, ProjetoResponse>
     {
     }
 }
