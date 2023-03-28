6063d8a2bcb02b1d6e03d1efd6d056d7452ae2da
Gabriela Calcabrine <gabrielacalcabrine@gmail.com>
2023-03-28 13:34:32 -0300

    feat: criacao interface ITarefaService

---
+++ Mda/Mda.Domain/Interfaces/ITarefaService.cs
@@ -0,0 +1,13 @@
+﻿using Mda.Domain.Contratos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mda.Domain.Interfaces
+{
+    public interface ITarefaService : IBaseService<TarefaRequest, TarefaResponse>
+    {
+    }
+}
