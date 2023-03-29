eda5b542db6957e2892367e194c9f115424231ff
Gabriela Calcabrine <gabrielacalcabrine@gmail.com>
2023-03-29 14:04:51 -0300

    feat : post AreaController criado

---
+++ Mda/Mda/Controllers/AreaController.cs
@@ -0,0 +1,34 @@
+﻿using Mda.Domain.Interfaces;
+using Mda.Domain.UsuarioContratos;
+using Mda.Service;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Mda.Api.Controllers
+{
+    public class AreaController : ControllerBase
+    {
+        private IAreaService _areaService;
+
+        public AreaController(IAreaService areaService)
+        {
+            _areaService = areaService;
+        }
+        [HttpPost]
+        [SwaggerOperation(Summary = "Cadastra uma nova Area no banco.", Description = "Retorna dados da Area.")]
+        [ProducesResponseType(201)]
+        public async Task<ActionResult<UsuarioResponse>> Post([FromBody] AreaRequestInicio area)
+        {
+            try
+            {
+                var result = await _areaService.Post(area);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+    }
+}
