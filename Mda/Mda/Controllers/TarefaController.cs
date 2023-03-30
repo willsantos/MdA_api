9d21139a052cd3a0b30a2653fef751ef8509e565
Gabriela Calcabrine <gabrielacalcabrine@gmail.com>
2023-03-30 11:56:34 -0300

    Feat : metodo Post TarefaController criado

---
+++ Mda/Mda/Controllers/TarefaController.cs
@@ -0,0 +1,40 @@
+﻿using Mda.Domain.Contratos;
+using Mda.Domain.Entities.Utils;
+using Mda.Domain.Interfaces;
+using Mda.Domain.UsuarioContratos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System.Data;
+
+namespace Mda.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TarefaController : ControllerBase
+    {
+        private ITarefaService _tarefaService;
+
+        public TarefaController(ITarefaService tarefaservice)
+        {
+            _tarefaService = tarefaservice;
+        }
+
+        [Authorize(Roles = ConstantUtil.PerfilLogadoNome)]
+        [HttpPost]
+        [SwaggerOperation(Summary = "Cadastra uma nova Tarefa no banco.", Description = "Retorna dados da Tarefa.")]
+        [ProducesResponseType(201)]
+        public async Task<ActionResult<AreaResponse>> Post([FromBody] TarefaRequest tarefa)
+        {
+            try
+            {
+                var result = await _tarefaService.Post(tarefa);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
