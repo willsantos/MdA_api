cf8309d4baa6f1d65359814a0e2a865767d8a91c
Gabriela Calcabrine <gabrielacalcabrine@gmail.com>
2023-03-30 11:17:22 -0300

    Feat : criacao metodo Post ProjetoController

---
+++ Mda/Mda/Controllers/ProjetoController.cs
@@ -0,0 +1,42 @@
+﻿using Mda.Domain.Contratos;
+using Mda.Domain.Entities.Utils;
+using Mda.Domain.Interfaces;
+using Mda.Domain.UsuarioContratos;
+using Mda.Service;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System.Data;
+
+namespace Mda.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProjetoController : ControllerBase
+    {
+        private readonly IProjetoService _projetoService;
+
+        public ProjetoController(IProjetoService projetoService)
+        {
+            _projetoService = projetoService;
+        }
+
+        [Authorize(Roles = ConstantUtil.PerfilLogadoNome)]
+        [HttpPost]
+        [SwaggerOperation(Summary = "Cadastra uma novo projeto no banco.", Description = "Retorna dados do projeto.")]
+        [ProducesResponseType(201)]
+        public async Task<ActionResult<ProjetoResponse>> Post([FromBody] ProjetoRequest request)
+        {
+            try
+            {
+                var result = await _projetoService.Post(request);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+    }
+}
