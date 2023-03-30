92011cec954ccf5f2854dd3ed09440ca1b7bfa15
Gabriela Calcabrine <gabrielacalcabrine@gmail.com>
2023-03-30 10:41:55 -0300

    Feat : ObjetivoController criado e método post

---
+++ Mda/Mda/Controllers/ObjetivoController.cs
@@ -0,0 +1,38 @@
+﻿using Mda.Domain.Entities.Utils;
+using Mda.Domain.Interfaces;
+using Mda.Domain.UsuarioContratos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Mda.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ObjetivoController : ControllerBase
+    {
+        private IObjetivoService _objetivoService;
+
+        public ObjetivoController(IObjetivoService objetivoService)
+        {
+            _objetivoService = objetivoService;
+        }
+
+        [Authorize(Roles = ConstantUtil.PerfilLogadoNome)]
+        [HttpPost]
+        [SwaggerOperation(Summary = "Cadastra uma novo Objetivo no banco.", Description = "Retorna dados do Objetivo.")]
+        [ProducesResponseType(201)]
+        public async Task<ActionResult<ObjetivoResponse>> Post([FromBody] ObjetivoRequest request)
+        {
+            try
+            {
+                var result = await _objetivoService.Post(request);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
