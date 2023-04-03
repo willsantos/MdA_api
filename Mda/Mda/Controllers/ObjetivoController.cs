b1b7842848ae707aeadd32792c8e2d2e2cdd5b73
Gabriela Calcabrine <gabrielacalcabrine@gmail.com>
2023-04-03 12:06:19 -0300

    Fix : Objetivo controller corrigido

--- Mda/Mda/Controllers/ObjetivoController.cs
+++ Mda/Mda/Controllers/ObjetivoController.cs
@@ -61,7 +61,7 @@ namespace Mda.Api.Controllers
             }
             catch (Exception ex)
             {
-                return BadRequest(ex.Message);
+                return NotFound(ex.Message);
             }
 
         }
@@ -86,7 +86,7 @@ namespace Mda.Api.Controllers
             }
             catch (Exception ex)
             {
-                return BadRequest(ex.Message);
+                return NotFound(ex.Message);
             }
 
         }
@@ -109,9 +109,17 @@ namespace Mda.Api.Controllers
                 var result = await _objetivoService.Put(objetivoAlteracao, id);
                 return Ok(result);
             }
-            catch (Exception ex)
+            catch (UnauthorizedAccessException ex)
+            {
+                return Unauthorized(ex.Message);
+            }
+            catch (ArgumentException ex)
             {
                 return BadRequest(ex.Message);
+            }
+            catch (Exception)
+            {
+                return Problem();
             }
 
         }
